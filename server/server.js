c1555b1778befee24b511601ebae2dd5a07d077a
ezamora <ezamorad@gmail.com>
2017-06-06 11:02:38 -0600

    add connect to a socket

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public')
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const port = process.env.PORT || '3000';
 
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('Client discnnected');
+    });
+});
 
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server running on port', port);
 });
