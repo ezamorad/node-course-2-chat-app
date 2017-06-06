968cd53b983e9d669fff9fe44c0f8a12972afca6
ezamora <ezamorad@gmail.com>
2017-06-06 16:19:02 -0600

    Broadcast

--- server/server.js
+++ server/server.js
@@ -23,6 +23,12 @@ io.on('connection', (socket) => {
     
     socket.on('createMessage', (message) => {
         console.log('Create message', message);
+        
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
     
     socket.on('disconnect', () => {
